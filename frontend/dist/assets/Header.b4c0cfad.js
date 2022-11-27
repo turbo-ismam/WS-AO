9bd6f41d5cf42d2c71068d72e436d50d05264970
Konrad Gomulka <konrad.gomulka@studio.unibo.it>
2022-11-27 13:52:52 +0100

    Remake pdf and delete useless files

--- frontend/dist/assets/Header.b4c0cfad.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./index.85a81505.js";const t=e('<nav class="px-4 lg:px-6 py-2.5 "><div class="flex flex-wrap justify-center items-center mx-auto max-w-screen-xl"><a class="flex items-center ml-20 pt-10"><img src="https://i.imgur.com/jtSCRjH.png" class="mr-3" alt="anonym.me Logo"></a></div></nav>'),s=()=>t.cloneNode(!0);export{s as default};
